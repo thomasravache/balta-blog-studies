aa32864b418d98e0cc2bce409c682a13dda5a28d
Thomas Ravache <thomasravache31@gmail.com>
2023-06-25 22:05:15 -0300

    feat: Entendendo o AppSettings

--- Program.cs
+++ Program.cs
@@ -1,6 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+var connectionString = app.Configuration.GetConnectionString("DefaultConnection");
+
+var secrets = new Secrets();
+// vai pegar informações do appsettings e jogar dentro da classe Secrets
+app.Configuration.GetSection("Secrets").Bind(secrets);
+
+app.MapGet("/", () => new {
+    ConnectionString = connectionString,
+    Secrets = secrets
+});
 
 app.Run();
+
+public class Secrets
+{
+    public string JwtTokenSecret { get; set; } = string.Empty;
+    public string ApiKey { get; set; } = string.Empty;
+    public string PrivateKey { get; set; } = string.Empty;
+}
