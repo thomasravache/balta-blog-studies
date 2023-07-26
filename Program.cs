6d6888301240c6a34365ea66ed2b06b978b38706
Thomas Ravache <thomasravache31@gmail.com>
2023-07-25 21:00:20 -0300

    feat: 79. Upload de Imagem para o Storage

--- Program.cs
+++ Program.cs
@@ -1,7 +1,12 @@
+using System.Text.RegularExpressions;
+using Azure.Storage.Blobs;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 var connectionString = app.Configuration.GetConnectionString("DefaultConnection");
+
+var azureStorageConnectionString = app.Configuration.GetValue<string>("AzureStorageConnectionString");
 
 var secrets = new Secrets();
 // vai pegar informações do appsettings e jogar dentro da classe Secrets
@@ -13,7 +18,30 @@ app.MapGet("/", () => new {
     ApiUrl = app.Configuration.GetValue<string>("ApiUrl")
 });
 
+app.MapPost("/", (Upload model) => {
+    UploadBase64Image(model.Image, "user-images");
+
+    return Results.Ok();
+});
+
 app.Run();
+
+string UploadBase64Image(string base64Image, string container)
+{
+    var fileName = Guid.NewGuid().ToString() + ".jpg";
+    var data = new Regex(@"^data:image\/[a-z]+;base64,").Replace(base64Image, "");
+
+    byte[] imageBytes = Convert.FromBase64String(data);
+    var blobClient = new BlobClient(azureStorageConnectionString, container, fileName);
+
+    using var stream = new MemoryStream(imageBytes);
+
+    blobClient.Upload(stream);
+
+    return blobClient.Uri.AbsoluteUri; // url final da imagem
+}
+
+public record Upload(string Image);
 
 public class Secrets
 {
